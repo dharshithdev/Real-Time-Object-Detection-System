ccc1193a9681d6a0fc7f20396b3dc3071b9cea83
Dharshith Dev <dharshithdevkp@gmail.com>
2026-02-11 11:57:54 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt') 
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        break
+
+    results = model.track(frame, persist=True)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLOv8 Real-Time Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
